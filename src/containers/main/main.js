532089e75e95f2d73216b8da00805714688e8fcf
cuong.nguyen <cuong.nguyen@boot.AI>
2023-09-25 18:06:56 +0700

    oke

--- src/containers/main/main.js
+++ src/containers/main/main.js
@@ -1,17 +1,32 @@
-import React, { useState, useEffect } from 'react';
-import 'boxicons/css/boxicons.min.css'; 
+import React, { useState, useEffect } from "react";
+import "boxicons/css/boxicons.min.css";
 import {
-  UserInfoContainer , UserName, Background, AvatarImage, AvatarContainer, DropdownMenu , DropdownItem , FlashingImage,
-  Header, Navbar, Container, LoginSection, Button, ButtonContainer, Item, SocialIcon, TextItem
-} from './mainStyle';
-import 'boxicons/css/boxicons.min.css';
-import axios from 'axios';
-import { Link, useHistory } from 'react-router-dom';
-import logout from './logout.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
-import arrowIcon from './arrow.png'; 
-import WordSearch from './wordsearch.png'
+  UserInfoContainer,
+  UserName,
+  Background,
+  AvatarImage,
+  AvatarContainer,
+  DropdownMenu,
+  DropdownItem,
+  FlashingImage,
+  Header,
+  Navbar,
+  Container,
+  LoginSection,
+  Button,
+  ButtonContainer,
+  Item,
+  SocialIcon,
+  TextItem,
+} from "./mainStyle";
+import "boxicons/css/boxicons.min.css";
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
+import logout from "./logout.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import arrowIcon from "./arrow.png";
+import WordSearch from "./wordsearch.png";
 
 function Main() {
   const [name_user, setUserData] = useState(null);
@@ -19,57 +34,64 @@ function Main() {
   const history = useHistory();
   useEffect(() => {
     // Lấy userData từ localStorage khi component được tạo
-    const userDataFromLocalStorage = JSON.parse(localStorage.getItem('user'));
+    const userDataFromLocalStorage = JSON.parse(localStorage.getItem("user"));
     if (userDataFromLocalStorage) {
       setUserData(userDataFromLocalStorage.name_user);
       setUserDataId(userDataFromLocalStorage.id);
     } else {
-      history.push('/login'); // Điều hướng đến màn hình đăng nhập
+      history.push("/login"); // Điều hướng đến màn hình đăng nhập
     }
   }, []); // Sử dụng [] để đảm bảo useEffect chỉ chạy một lần khi component được tạo
   const avatarUrl = `http://127.0.0.1:5000/api/v1/picture/avatar/${user_id}`;
   const handleLogout = () => {
-    localStorage.removeItem('user')
-    localStorage.removeItem('accessToken')
-    localStorage.removeItem('refreshToken')
+    localStorage.removeItem("user");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
 
-    window.location.href = '/login'
+    window.location.href = "/login";
   };
   const [TurnAndCauDo, setData] = useState({});
 
   const handlePlayClick = async () => {
     try {
-      const access_token = localStorage.getItem('accessToken');
+      const access_token = localStorage.getItem("accessToken");
       // Thực hiện yêu cầu POST đến API để lấy danh sách 5 id
-      const response = await axios.get('http://127.0.0.1:5000/api/v1/luot_choi',{
+      const response = await axios.get(
+        "http://127.0.0.1:5000/api/v1/luot_choi",
+        {
           headers: {
-          Authorization: `Bearer ${access_token}`,
+            Authorization: `Bearer ${access_token}`,
           },
-        },{});
+        },
+        {}
+      );
       const respon = response.data.data;
       setData(respon);
 
-      history.push(`/playgame`, {TurnAndCauDo});
+      history.push(`/playgame`, { TurnAndCauDo: respon });
     } catch (error) {
-      console.error('Error:', error);
+      console.error("Error:", error);
     }
   };
   return (
     <div>
       <Header>
         <Navbar>
-          <a href="/main"><i className='bx bxl-xing'></i>Word Scamble</a>
+          <a href="/main">
+            <i className="bx bxl-xing"></i>Word Scamble
+          </a>
         </Navbar>
         <UserInfoContainer>
-        <UserName>{name_user}</UserName>
+          <UserName>{name_user}</UserName>
 
-        <AvatarContainer>
+          <AvatarContainer>
             <AvatarImage src={avatarUrl} alt="Avatar" />
             <DropdownMenu>
               <DropdownItem>Cài Đặt</DropdownItem>
               <DropdownItem>Tài Khoản</DropdownItem>
               <DropdownItem onClick={handleLogout}>
-              <img src={logout} alt="Logout" /></DropdownItem>
+                <img src={logout} alt="Logout" />
+              </DropdownItem>
             </DropdownMenu>
           </AvatarContainer>
         </UserInfoContainer>
@@ -77,22 +99,39 @@ function Main() {
       <Background></Background>
       <Container>
         <LoginSection>
-            <ButtonContainer>
+          <ButtonContainer>
             <FlashingImage src={arrowIcon} alt="Mũi tên" width="50" />
-            <Button onClick={handlePlayClick}>Play <FontAwesomeIcon icon={faPlay} className="play-icon" /></Button>
-            <Button>History<FontAwesomeIcon icon={faPlay} className="play-icon" /></Button>
+            <Button onClick={handlePlayClick}>
+              Play <FontAwesomeIcon icon={faPlay} className="play-icon" />
+            </Button>
+            <Button>
+              History
+              <FontAwesomeIcon icon={faPlay} className="play-icon" />
+            </Button>
           </ButtonContainer>
         </LoginSection>
         <Item>
-          <h2 className="logo"><i className='bx bxl-xing'></i>Word Scamble</h2>
+          <h2 className="logo">
+            <i className="bx bxl-xing"></i>Word Scamble
+          </h2>
           <TextItem>
             <img width="350" height="250" src={WordSearch} alt="Word Search" />
             <SocialIcon>
-              <a href="https://www.facebook.com/cuong.9402/"><i className='bx bxl-facebook'></i></a>
-              <a href="https://www.facebook.com/cuong.9402/"><i className='bx bxl-twitter'></i></a>
-              <a href="https://www.youtube.com/watch?v=fLRf8JqSX8A"><i className='bx bxl-youtube'></i></a>
-              <a href="https://www.instagram.com/cuong.9402/"><i className='bx bxl-instagram'></i></a>
-              <a href="https://www.facebook.com/cuong.9402/"><i className='bx bxl-linkedin'></i></a>
+              <a href="https://www.facebook.com/cuong.9402/">
+                <i className="bx bxl-facebook"></i>
+              </a>
+              <a href="https://www.facebook.com/cuong.9402/">
+                <i className="bx bxl-twitter"></i>
+              </a>
+              <a href="https://www.youtube.com/watch?v=fLRf8JqSX8A">
+                <i className="bx bxl-youtube"></i>
+              </a>
+              <a href="https://www.instagram.com/cuong.9402/">
+                <i className="bx bxl-instagram"></i>
+              </a>
+              <a href="https://www.facebook.com/cuong.9402/">
+                <i className="bx bxl-linkedin"></i>
+              </a>
             </SocialIcon>
           </TextItem>
         </Item>
--- src/containers/playgame/playgame.js
+++ src/containers/playgame/playgame.js
@@ -1,57 +1,106 @@
-import React, { useState, useEffect } from 'react';
-import 'boxicons/css/boxicons.min.css'; 
+import React, { useState, useEffect } from "react";
+import "boxicons/css/boxicons.min.css";
 import {
-  UserInfoContainer , UserName, Background, AvatarImage, AvatarContainer, DropdownMenu , DropdownItem , FlashingImage,
-  Header, Navbar, Container, LoginSection, Button, ButtonContainer, Item, SocialIcon, TextItem
-} from './playgameStyle';
-import 'boxicons/css/boxicons.min.css';
-import axios from 'axios';
-import { Link, useHistory } from 'react-router-dom';
-import logout from './logout.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faPlay } from '@fortawesome/free-solid-svg-icons';
-import arrowIcon from './arrow.png'; 
-import WordSearch from './wordsearch.png'
+  UserInfoContainer,
+  UserName,
+  Background,
+  AvatarImage,
+  AvatarContainer,
+  DropdownMenu,
+  DropdownItem,
+  FlashingImage,
+  Header,
+  Navbar,
+  Container,
+  LoginSection,
+  Button,
+  ButtonContainer,
+  Item,
+  SocialIcon,
+  TextItem,
+} from "./playgameStyle";
+import "boxicons/css/boxicons.min.css";
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
+import logout from "./logout.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlay } from "@fortawesome/free-solid-svg-icons";
+import arrowIcon from "./arrow.png";
+import WordSearch from "./wordsearch.png";
+import { useLocation } from "react-router-dom/cjs/react-router-dom.min";
+
+function setCharAt(str, index, chr) {
+  if (index > str.length - 1) return str;
+  return str.substring(0, index) + chr + str.substring(index + 1);
+}
 
 function PlayGame() {
+  const location = useLocation();
+
+  const { de_bai, turn_id } = location.state.TurnAndCauDo;
+
+  const [listQuestions, setListQuestions] = useState(de_bai);
+
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+
+  const [currentAnswer, setCurrentAnswer] = useState("");
+
   const [name_user, setUserData] = useState(null);
   const [user_id, setUserDataId] = useState(null);
   const history = useHistory();
 
   useEffect(() => {
     // Lấy userData từ localStorage khi component được tạo
-    const userDataFromLocalStorage = JSON.parse(localStorage.getItem('user'));
+    const userDataFromLocalStorage = JSON.parse(localStorage.getItem("user"));
     if (userDataFromLocalStorage) {
       setUserData(userDataFromLocalStorage.name_user);
       setUserDataId(userDataFromLocalStorage.id);
     } else {
-      history.push('/login'); // Điều hướng đến màn hình đăng nhập
+      history.push("/login"); // Điều hướng đến màn hình đăng nhập
     }
   }, []); // Sử dụng [] để đảm bảo useEffect chỉ chạy một lần khi component được tạo
   const avatarUrl = `http://127.0.0.1:5000/api/v1/picture/avatar/${user_id}`;
   const handleLogout = () => {
-    localStorage.removeItem('user')
-    localStorage.removeItem('accessToken')
-    localStorage.removeItem('refreshToken')
+    localStorage.removeItem("user");
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("refreshToken");
 
-    window.location.href = '/login'
+    window.location.href = "/login";
   };
+
+  const handleClickText = (text) => {
+    setCurrentAnswer((prev) => (prev += text));
+  };
+
+  const handleClickTextAnswer = (index) => {
+    setCurrentAnswer((prev) => {
+      const newText = setCharAt(prev, index, "");
+      console.log(prev, index, newText);
+      return newText;
+    });
+  };
+
+  const currentQuestion = listQuestions[currentQuestionIndex];
+
   return (
     <div>
       <Header>
         <Navbar>
-          <a href="/main"><i className='bx bxl-xing'></i>Word Scamble</a>
+          <a href="/main">
+            <i className="bx bxl-xing"></i>Word Scamble
+          </a>
         </Navbar>
         <UserInfoContainer>
-        <UserName>{name_user}</UserName>
+          <UserName>{name_user}</UserName>
 
-        <AvatarContainer>
+          <AvatarContainer>
             <AvatarImage src={avatarUrl} alt="Avatar" />
             <DropdownMenu>
               <DropdownItem>Cài Đặt</DropdownItem>
               <DropdownItem>Tài Khoản</DropdownItem>
               <DropdownItem onClick={handleLogout}>
-              <img src={logout} alt="Logout" /></DropdownItem>
+                <img src={logout} alt="Logout" />
+              </DropdownItem>
             </DropdownMenu>
           </AvatarContainer>
         </UserInfoContainer>
@@ -59,12 +108,56 @@ function PlayGame() {
       <Background></Background>
       <Container>
         <LoginSection>
-            <ButtonContainer>
-              <FlashingImage src={arrowIcon} alt="Mũi tên" width="50" />
-            </ButtonContainer>
+          <ButtonContainer>
+            <FlashingImage src={arrowIcon} alt="Mũi tên" width="50" />
+          </ButtonContainer>
         </LoginSection>
         <Item>
-          <h2 className="logo"><i className='bx bxl-xing'></i>Word Scamble</h2>
+          <div
+            style={{
+              display: "flex",
+              gap: 10,
+            }}
+          >
+            {currentQuestion.de_bai.map((text) => (
+              <span
+                style={{
+                  display: "inline-block",
+                  padding: "10px",
+                  background: "red",
+                  borderRadius: 5,
+                }}
+                onClick={() => handleClickText(text)}
+              >
+                {text}
+              </span>
+            ))}
+          </div>
+
+          <div
+            style={{
+              display: "flex",
+              gap: 10,
+            }}
+          >
+            {currentQuestion.de_bai.map((_, index) => {
+              const currentText = currentAnswer[index];
+
+              return (
+                <span
+                  style={{
+                    display: "inline-block",
+                    padding: "10px",
+                    background: "blue",
+                    borderRadius: 5,
+                  }}
+                  onClick={() => handleClickTextAnswer(index)}
+                >
+                  {currentText}
+                </span>
+              );
+            })}
+          </div>
           <TextItem>
             <img width="350" height="250" src={WordSearch} alt="Word Search" />
           </TextItem>
