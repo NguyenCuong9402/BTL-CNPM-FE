75950ba6908c144faaae8a1a6b07a997b9b101fa
cuong.nguyen <cuong.nguyen@boot.AI>
2023-10-26 15:12:16 +0700

    okeeee

--- src/containers/login/Login.js
+++ src/containers/login/Login.js
@@ -1,19 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import './loginStyle.css'; // Import file CSS   okeeeeee
-import 'boxicons/css/boxicons.min.css'; // Import thư viện icons
-import axios from 'axios';
-import { Link, useHistory  } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import "./loginStyle.css"; // Import file CSS   okeeeeee
+import "boxicons/css/boxicons.min.css"; // Import thư viện icons
+import axios from "axios";
+import { Link, useHistory } from "react-router-dom";
 import Modal from "../../modal";
 
+import {
+  SocialIcon,
+  SocialIconItem,
+  SocialLink,
+  SocialIconItemHover,
+  Item1Wrapper, StyledHeading, Header, Navbar
+} from "./loginStyled";
+
 function Login() {
-  const [email, setEmail] = useState('');
-  const [emailforget, setEmailForget] = useState('');
+  const [email, setEmail] = useState("");
+  const [emailforget, setEmailForget] = useState("");
   const [isPush, setPush] = useState(false);
 
-  const [password, setPassword] = useState('');
+  const [password, setPassword] = useState("");
   const history = useHistory();
   const [isModalOpen, setModalOpen] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
+  const [modalMessage, setModalMessage] = useState("");
   const [showForgotPassword, setShowForgotPassword] = useState(false);
   const handleEmailChange = (e) => {
     setEmail(e.target.value);
@@ -28,44 +36,45 @@ function Login() {
   };
   useEffect(() => {
     // Kiểm tra xem trong localStorage có user không
-    const userData = JSON.parse(localStorage.getItem('user'));
+    const userData = JSON.parse(localStorage.getItem("user"));
     if (userData) {
-      history.push('/index');
+      history.push("/index");
     }
   }, [history]);
- 
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    axios.post('http://127.0.0.1:5000/api/v1/user/login', {
-      email: email,
-      password: password,
-      admin: false
-    })
+    axios
+      .post("http://127.0.0.1:5000/api/v1/user/login", {
+        email: email,
+        password: password,
+        admin: false,
+      })
       .then(function (response) {
         if (response.data.message.status === "success") {
-          localStorage.setItem('accessToken', response.data.data.access_token);
-          localStorage.setItem('refreshToken', response.data.data.refresh_token);
-          localStorage.setItem('user', JSON.stringify(response.data.data.user));
-          window.location.href = '/index';
+          localStorage.setItem("accessToken", response.data.data.access_token);
+          localStorage.setItem(
+            "refreshToken",
+            response.data.data.refresh_token
+          );
+          localStorage.setItem("user", JSON.stringify(response.data.data.user));
+          window.location.href = "/index";
         }
         if (response.data.message.status === "error") {
           setModalMessage(response.data.message.text);
           setModalOpen(true);
         }
-
       })
       .catch(function (error) {
         console.error(error);
-        alert('Tài khoản không tồn tại');
+        alert("Tài khoản không tồn tại");
       });
   };
-
 
   const handleCloseModal = () => {
     setModalOpen(false);
     if (isPush === true) {
-      setShowForgotPassword(false)
+      setShowForgotPassword(false);
     }
   };
 
@@ -73,16 +82,16 @@ function Login() {
     setShowForgotPassword(true);
   };
   const handleSendPasswordEmail = () => {
-    axios.post('http://127.0.0.1:5000/api/v1/user/send_pass_email', {
-      email: emailforget,
-    })
+    axios
+      .post("http://127.0.0.1:5000/api/v1/user/send_pass_email", {
+        email: emailforget,
+      })
       .then(function (response) {
         if (response.data.message.status === "success") {
           setModalMessage(response.data.message.text);
           setModalOpen(true);
-          setPush(true)
-          setEmailForget('')
-
+          setPush(true);
+          setEmailForget("");
         }
         if (response.data.message.status === "error") {
           setModalMessage(response.data.message.text);
@@ -91,85 +100,123 @@ function Login() {
       })
       .catch(function (error) {
         console.error(error);
-        alert('Có lỗi xảy ra khi gửi email khôi phục mật khẩu');
+        alert("Có lỗi xảy ra khi gửi email khôi phục mật khẩu");
       });
   };
 
   return (
     <div>
-      <div className='body1'>
-      {/* LOGIN FORM CREATION */}
-      <div className="background1"></div>
-      <div className="container">
-        <div className="item1">
-          <h2 className="logo"><i className='bx bxl-xing'></i>Mua sắm thả ga</h2>
-            <h2>Welcome! <br /></h2>
-            <p>Hãy tiêu tới bến</p>
-            <div className="social-icon">
-              <a href="https://www.facebook.com/cuong.9402/"><i className='bx bxl-facebook'></i></a>
-              <a href="https://www.facebook.com/cuong.9402/"><i className='bx bxl-twitter'></i></a>
-              <a href="https://www.youtube.com/watch?v=fLRf8JqSX8A"><i className='bx bxl-youtube'></i></a>
-              <a href="https://www.instagram.com/cuong.9402/"><i className='bx bxl-instagram'></i></a>
-              <a href="https://www.facebook.com/cuong.9402/"><i className='bx bxl-linkedin'></i></a>
+      <div className="body1">
+        {/* LOGIN FORM CREATION */}
+        <div className="background1"></div>
+        <Header>
+        <Navbar>
+          <a href="/index"  style={{ fontSize: '26px' }}>
+            <i className="bx bxl-xing"></i>Home
+          </a>
+        </Navbar>
+        </Header>
+        <div className="container">
+          <Item1Wrapper>
+            <StyledHeading>Chào mừng đến với Shop</StyledHeading>
+            <SocialIcon>
+              <SocialLink href="https://www.facebook.com/cuong.9402">
+                <SocialIconItem className="bx bxl-facebook" />
+              </SocialLink>
+              <SocialLink href="https://www.facebook.com/cuong.9402">
+                <SocialIconItem className="bx bxl-twitter" />
+              </SocialLink>
+              <SocialLink href="https://www.youtube.com/watch?v=fLRf8JqSX8A">
+                <SocialIconItemHover className="bx bxl-youtube" />
+              </SocialLink>
+              <SocialLink href="https://www.instagram.com/cuong.9402">
+                <SocialIconItem className="bx bxl-instagram" />
+              </SocialLink>
+              <SocialLink href="https://www.facebook.com/cuong.9402">
+                <SocialIconItem className="bx bxl-linkedin" />
+              </SocialLink>
+            </SocialIcon>
+            </Item1Wrapper>
+          <div className="login-section">
+            <div className="form-box login">
+              <form action="" onSubmit={handleSubmit}>
+                <h2>Sign In</h2>
+                <div className="input-box">
+                  <span className="icon">
+                    <i className="bx bxs-envelope"></i>
+                  </span>
+                  <input
+                    type="email"
+                    required
+                    value={email}
+                    onChange={handleEmailChange}
+                  />
+                  <label>Email</label>
+                </div>
+                <div className="input-box">
+                  <span className="icon">
+                    <i className="bx bxs-lock-alt"></i>
+                  </span>
+                  <input
+                    type="password"
+                    required
+                    value={password}
+                    onChange={handlePasswordChange}
+                  />
+                  <label>Password</label>
+                </div>
+                <div className="remember-password">
+                  <label>
+                    <input type="checkbox" />
+                    Remember Me
+                  </label>
+                  <a onClick={handleForgotPasswordClick}>Forget Password</a>
+                </div>
+                <button className="btn">Log In</button>
+                <div className="create-account">
+                  <p>
+                    Create A New Account?{" "}
+                    <Link to="/Register" className="register-link">
+                      Sign Up
+                    </Link>
+                  </p>
+                </div>
+              </form>
             </div>
-        </div>
-        <div className="login-section">
-          <div className="form-box login">
-            <form action="" onSubmit={handleSubmit}>
-              <h2>Sign In</h2>
-              <div className="input-box">
-                <span className="icon"><i className='bx bxs-envelope'></i></span>
-                <input
-                  type="email"
-                  required
-                  value={email}
-                  onChange={handleEmailChange}
-                />
-                <label>Email</label>
-              </div>
-              <div className="input-box">
-                <span className="icon"><i className='bx bxs-lock-alt'></i></span>
-                <input
-                  type="password"
-                  required
-                  value={password}
-                  onChange={handlePasswordChange}
-                />
-                <label>Password</label>
-              </div>
-              <div className="remember-password">
-                <label><input type="checkbox" />Remember Me</label>
-                <a onClick={handleForgotPasswordClick}>Forget Password</a>
-              </div>
-              <button className="btn">Log In</button>
-              <div className="create-account">
-                <p>Create A New Account? <Link to="/Register" className="register-link">Sign Up</Link></p>
-              </div>
-            </form>
           </div>
-        </div>
-        {showForgotPassword && (
-          <div>
-          <div className="overlay"></div>
-          <div className="forgot-password-form">
-          <h2 style={{ color: 'white' }}>Nhập Email</h2>          
-          <div className="input-box">
-            <input
-              type="email"
-              required
-              value={emailforget}
-              onChange={handleEmailForgetChange}
-              placeholder="Điền email"
-            />
-          </div>
-          <button className="btn1" onClick={handleSendPasswordEmail}>Send Email</button>
-          <button className="btn1" onClick={() => setShowForgotPassword(false)}>Cancel</button>
-          </div>
-          </div>
+          {showForgotPassword && (
+            <div>
+              <div className="overlay"></div>
+              <div className="forgot-password-form">
+                <h2 style={{ color: "white" }}>Nhập Email</h2>
+                <div className="input-box">
+                  <input
+                    type="email"
+                    required
+                    value={emailforget}
+                    onChange={handleEmailForgetChange}
+                    placeholder="Điền email"
+                  />
+                </div>
+                <button className="btn1" onClick={handleSendPasswordEmail}>
+                  Send Email
+                </button>
+                <button
+                  className="btn1"
+                  onClick={() => setShowForgotPassword(false)}
+                >
+                  Cancel
+                </button>
+              </div>
+            </div>
           )}
+        </div>
+        <Modal
+          isOpen={isModalOpen}
+          message={modalMessage}
+          onClose={handleCloseModal}
+        />
       </div>
-      <Modal isOpen={isModalOpen} message={modalMessage} onClose={handleCloseModal} />
-    </div>
     </div>
   );
 }
---
+++ src/containers/login/loginStyled.js
@@ -0,0 +1,98 @@
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+
+export const SocialIcon = styled.div`
+  display: flex;
+  align-items: center; /* Căn giữa theo chiều dọc nếu cần */
+`;
+
+export const SocialLink = styled.a`
+  color: #fff;
+  font-size: 24px;
+  margin-left: 10px;
+  cursor: pointer;
+  transition: 0.5s ease;
+  text-decoration: none; /* Loại bỏ gạch chân trên các liên kết */
+`;
+
+export const SocialIconItem = styled.i`
+  display: flex;
+`;
+
+export const SocialIconItemHover = styled(SocialIconItem)`
+  &:hover {
+    transform: translateY(-10px);
+  }
+`;
+
+export const Item1Wrapper = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 58%;
+  height: 100%;
+  color: #fff;
+  background: transparent;
+  padding: 80px;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const StyledHeading = styled.h2`
+  font-size: 36px;
+  color: #ff69b4;
+  text-shadow: 2px 2px 4px rgba(255, 105, 180, 0.5);
+  background: linear-gradient(
+    to right,
+    #56ccf2,
+    #2f80ed
+  ); /* Thay đổi màu nền ở đây */
+  -webkit-background-clip: text;
+  color: transparent;
+  display: inline-block;
+`;
+
+export const Header = styled.header`
+  position: fixed;
+  top: 0;
+  
+  left: 0;
+  width: 100%;
+  padding: 25px 13%;
+  background: black; /* Đặt màu nền thành đen */
+  color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  z-index: 100;
+`;
+
+export const Navbar = styled.nav`
+  a {
+    position: relative;
+    font-size: 16px;
+    color: #fff;
+    margin-right: 30px;
+    text-decoration: none;
+
+    &::after {
+      content: "";
+      position: absolute;
+      left: 0;
+      width: 100%;
+      height: 2px;
+      background: #fff;
+      bottom: -5px;
+      border-radius: 5px;
+      transform: translateY(10px);
+      opacity: 0;
+      transition: .5s ease;
+    }
+
+    &:hover::after {
+      transform: translateY(0);
+      opacity: 1;
+    }
+  }
+`;
+
